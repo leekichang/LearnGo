0b64b70ec9f168d6e4aa65c4b8af37d63c0cf562
leekichang <1kichang@yonsei.ac.kr>
2020-12-31 21:55:30 +0900

    dictionary part two

--- Dictionary/main.go
+++ Dictionary/main.go
@@ -8,10 +8,23 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{"first": "First Word"}
-	definition, err := dictionary.Search("second")
-	if err != nil {
-		fmt.Println(err)
+	definition, err1 := dictionary.Search("second")
+	if err1 != nil {
+		fmt.Println(err1)
 	} else {
 		fmt.Println(definition)
+	}
+	word := "hello"
+	def := "Greeting"
+	err2 := dictionary.Add(word, def)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	hello, _ := dictionary.Search("hello")
+	fmt.Println("found", word, "definition", hello)
+
+	err3 := dictionary.Add(word, def)
+	if err3 != nil {
+		fmt.Println(err3)
 	}
 }
--- Dictionary/mydict/mydict.go
+++ Dictionary/mydict/mydict.go
@@ -3,6 +3,7 @@ package mydict
 import "errors"
 
 var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That word already exusts")
 
 //Dictionary tyoe
 type Dictionary map[string]string
@@ -14,4 +15,16 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+//Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
 }
