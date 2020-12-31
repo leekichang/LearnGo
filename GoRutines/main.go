cfd169b6b610d6a3f174bde1d18db42a9b36c7ba
leekichang <1kichang@yonsei.ac.kr>
2021-01-01 01:47:08 +0900

    GoRutines

---
+++ GoRutines/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go sexyCount("nico")
+	go sexyCount("FLYN")
+	time.Sleep(time.Second * 5)
+}
+
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
+	}
+}
