849f1779ea6e0862a4514bbc3ac31a76ee491bfd
leekichang <1kichang@yonsei.ac.kr>
2021-01-01 07:58:21 +0900

    getPages

--- JobScrapper/main.go
+++ JobScrapper/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -11,10 +12,19 @@ import (
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
-	getPages()
+	totalPages := getPages()
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
+	}
+}
+
+func getPage(page int) {
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("Requesting ", pageURL)
 }
 
 func getPages() int {
+	page := 0
 	resp, err := http.Get(baseURL)
 	checkErr(err)
 	checkCode(resp)
@@ -23,10 +33,10 @@ func getPages() int {
 
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	checkErr(err)
-	doc.Find(".pagination").Each()
-	fmt.Println(doc)
-
-	return 0
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		page = s.Find("a").Length()
+	})
+	return page
 }
 
 func checkErr(err error) {
