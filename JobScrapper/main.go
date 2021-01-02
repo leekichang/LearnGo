f60723f36b03d11ef54cb2f52c1242ad127b4e8b
leekichang <1kichang@yonsei.ac.kr>
2021-01-02 09:06:34 +0900

    File Download

--- JobScrapper/main.go
+++ JobScrapper/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"os"
 	"strings"
 
 	"github.com/labstack/echo"
 	"github.com/leekichang/LearnGo/JobScrapper/scrapper"
 )
 
+const fileName string = "jobs.csv"
+
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 
 func handleScrape(c echo.Context) error {
+	defer os.Remove("/mnt/c/Users/Administrator/Downloads" + fileName)
 	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
-	return nil
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
 }
 
 func main() {
